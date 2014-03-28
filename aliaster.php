ba637aa369390476cf1bc5410e40eb328997108e
kurage <kurage@gekkai.org>
2014-03-28 19:55:45 +0900

    "このプラグインを有効化する際に、1文字分の予期しない出力が生成されました。"のエラー

--- aliaster.php
+++ aliaster.php
@@ -26,9 +26,6 @@ License: GPL2
 	with this program; if not, write to the Free Software Foundation, Inc.,
 	51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
-?>
-
-<?php
 
 $aliaster_substitute;
 $alister_enable = false;
